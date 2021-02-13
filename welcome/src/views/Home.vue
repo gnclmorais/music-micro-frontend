a69da63037f7af115f27bf5a4b50cd3bbe64ca55
Gonçalo Morais <gnclmorais@gmail.com>
2021-02-13 21:24:52 +0000

    Add latest touches to homepage
    Not a final version, but good enough for now.

--- welcome/src/views/Home.vue
+++ welcome/src/views/Home.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="home flex-grow px-6 py-5 flex items-center justify-center">
     <div>
-      <h1>Red Records</h1>
-      <h2>music label</h2>
+      <h2 class="text-5xl">
+        The <strong>music label</strong> for <strong>vinyls</strong>
+      </h2>
+
+      <div class="location flex items-center flex-col mt-20">
+        <span class="pb-3 text-3xl">
+          Operating in
+        </span>
+        <div class="locations flex">
+          <div class="usa text-center px-3 text-2xl">
+            New York
+            <br>
+            🇺🇸
+          </div>
+          <div class="uk text-center px-3 text-2xl">
+            London
+            <br>
+            🇬🇧
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -15,6 +34,6 @@ export default {
 
 <style>
 .home {
-  
+
 }
 </style>
