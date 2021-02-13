ba605112d0806fbd0730811a7e0e033e23fa07b1
Gonçalo Morais <gnclmorais@gmail.com>
2021-02-13 15:33:24 +0000

    Change about to signin

--- welcome/src/App.vue
+++ welcome/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-    
+    <router-link to="/signin">Signin</router-link>
+
     <router-view />
   </div>
 </template>
