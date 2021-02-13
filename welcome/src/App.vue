78049172d75009f98687aad72024b4fa6b52e91d
Gonçalo Morais <gnclmorais@gmail.com>
2021-02-13 18:29:29 +0000

    First visual setup
    Work a bit on a header & navbar combo, to get a feeling of how the
    page should look.

--- welcome/src/App.vue
+++ welcome/src/App.vue
@@ -1,31 +1,36 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/signin">Signin</router-link>
+  <div class="body">
+    <header class="bg-red-600 text-white flex px-6 py-5">
+      <div id="logo" class="flex-shrink logo">
+        <router-link to="/">Red Records</router-link>
+      </div>
+
+      <nav id="nav" class="ml-20 flex-grow flex items-center">
+        <ul class="flex">
+          <li class="nav-item">
+            <router-link to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link to="/signin">Signin</router-link>
+          </li>
+        </ul>
+      </nav>
+    </header>
 
     <router-view />
   </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.logo {
+  font-family: Impact;
+  font-size: 2em;
+  text-transform: uppercase;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.nav-item +.nav-item::before {
+  content: "|";
+  display: inline;
+  margin: 0 1em;
 }
 </style>
--- welcome/src/views/Home.vue
+++ welcome/src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home px-6 py-5">
+    <h1>Red Records</h1>
+    <h2>music label</h2>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
