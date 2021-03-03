4b9119f73d4ee5ac6d508f133fba3e9b0e6b670b
Gonçalo Morais <gnclmorais@gmail.com>
2021-03-03 21:15:50 +0000

    Setup pulic API to toggle frontends

--- bootstrap/main.js
+++ bootstrap/main.js
@@ -1,58 +1,101 @@
-const { protocol, host, pathname } = new URL(window.location.href);
-const appToLoad = pathname.replace(/\//g, '');
 const availableApps = {
   hello: '/mfe/welcome',
   play: '/mfe/music',
 };
 
-console.log(`Fetching ${host}${availableApps[appToLoad]}…`);
-
+const unmountClass = 'unmount';
 const injectScript = (parent, src, innerHTML) => {
   const script = document.createElement('SCRIPT');
 
-  script.src = src;
+  if (src) script.src = src;
   script.innerHTML = innerHTML;
+  script.classList.add(unmountClass);
 
   return parent.appendChild(script);
 }
 
-const newPath = availableApps[appToLoad];
-fetch(`http://${host}${newPath}`)
-  .then(response => response.text())
-  .then((html) => {
-    const head = document.head;
-    const body = document.body;
+function unmount() {
+  const head = document.head;
+  const body = document.body;
 
-    // Replace the content of the current <head> with a <base> element
-    const baseEl = `<base href="${newPath}/">`;
-    head.innerHTML = baseEl;
+  // Empty head
+  const headChildren = head.querySelectorAll(`.${unmountClass}`);
+  Array.from(headChildren).forEach((child) => head.removeChild(child));
 
-    // Parse the page received
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(html, 'text/html');
+  // Empty body
+  body.innerHTML = '';
+}
 
-    // Import <head> first
-    const headChildren = Array.from(doc.querySelector('head').childNodes);
-    headChildren.forEach(node => {
-      if (node.nodeName === 'SCRIPT') {
-        injectScript(head, node.src);
-      } else {
-        head.appendChild(document.adoptNode(node));
-      }
-    });
+function mount(frontend) {
+  const { host } = new URL(window.location.href);
+  const base = availableApps[frontend];
 
-    // Import <body> next
-    const srcRegex = /<script.*?src="(.*?)"/;
-    const bodyChildren = Array.from(doc.querySelector('body').childNodes);
-    bodyChildren.forEach(node => {
-      if (node.nodeName === 'SCRIPT') {
-        // For some reason, <base> doesn't change the `src` as expected,
-        // so some magic is required to get it…
-        const src = (srcRegex.exec(node.outerHTML) || [])[1];
-        injectScript(body, src, node.innerHTML);
-      } else {
-        body.appendChild(document.adoptNode(node));
-      }
-    });
-  })
-  .catch(console.log);
+  console.log(`Fetching ${base}…`);
+
+  fetch(`http://${host}${base}`)
+    .then(response => response.text())
+    .then((html) => {
+      const head = document.head;
+      const body = document.body;
+
+      // Replace the content of the current <head> with a <base> element
+      const baseEl = `<base href="${base}/">`;
+      head.innerHTML = baseEl;
+
+      // Parse the page received
+      const parser = new DOMParser();
+      const doc = parser.parseFromString(html, 'text/html');
+
+      // Import <head> first
+      const headChildren = Array.from(doc.querySelector('head').childNodes);
+      headChildren.forEach(node => {
+        if (node.nodeName === 'SCRIPT') {
+          injectScript(head, node.src);
+        } else {
+          node.classList.add(unmountClass);
+          head.appendChild(document.adoptNode(node));
+        }
+      });
+
+      // Import <body> next
+      const srcRegex = /<script.*?src="(.*?)"/;
+      const bodyChildren = Array.from(doc.querySelector('body').childNodes);
+      bodyChildren.forEach(node => {
+        if (node.nodeName === 'SCRIPT') {
+          // For some reason, <base> doesn't change the `src` as expected,
+          // so some magic is required to get it…
+          const src = (srcRegex.exec(node.outerHTML) || [])[1];
+          injectScript(body, src, node.innerHTML);
+        } else {
+          body.appendChild(document.adoptNode(node));
+        }
+      });
+    })
+    .catch(console.log);  
+}
+
+function navigateTo(frontend, { back = false } = {}) {
+  unmount();
+
+  if (back) {
+    history.replaceState({ id: frontend }, '', `/${frontend}`);
+  } else {
+    history.pushState({ id: frontend }, '', `/${frontend}`);
+  }
+
+  mount(frontend);
+}
+
+// Set up our public API
+if (!window.bootstrap) {
+  window.bootstrap = { router: { navigateTo } };
+}
+
+const { pathname } = new URL(window.location.href);
+const frontend = pathname.replace(/\//g, '');
+
+// Navigate to our first page
+navigateTo(frontend);
+
+// Set up back and forward control
+window.addEventListener('popstate', ({ state: { id: frontend } }) => navigateTo(frontend, { back: true }));
